0e393fa643b3c92b94d6919ee2239e4fdfeb1e9b
X/O <tymontrybura@gmail.com>
2024-05-07 13:57:08 +0200

    Initial commit

---
+++ src/main/java/net/xo/xo_test_mod/XoTestMod.java
@@ -0,0 +1,16 @@
+package net.xo.xo_test_mod;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class XoTestMod implements ModInitializer {
+	public static final String MOD_ID = "xo_test_mod";
+    public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+		LOGGER.info("Custom mod loaded");
+	}
+}
---
+++ src/main/java/net/xo/xo_test_mod/XoTestModClient.java
@@ -0,0 +1,10 @@
+package net.xo.xo_test_mod;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class XoTestModClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient(){
+
+    }
+}
